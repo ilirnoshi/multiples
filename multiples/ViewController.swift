d6e972eeb1285787b587123cfe3656614abf1db4
Ilir Camaj <ilirc@ilirs-imac.t-com.me>
2016-01-15 17:33:19 +0000

    I just saved the project

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Properties
+    let maxAmount: Int = 50
+    var currentAmount: Int = 0
+    var multiplier: Int = 0
+    
+    // Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var theMltpl: UITextField!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var addLbl: UILabel!
+    
+    @IBAction func playButtonPressed (sender: UIButton!) {
+        if theMltpl.text != nil && theMltpl.text != "" {
+            logoImg.hidden = true
+            playBtn.hidden = true
+            theMltpl.hidden = true
+            addBtn.hidden = false
+            addLbl.hidden = false
+            
+            multiplier = Int(theMltpl.text!)!
+            currentAmount = 0
+            addLbl.text = "Press add to add!"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addButtonPressed (sender: UIButton!) {
+        addLbl.text = "\(currentAmount) + \(multiplier) = \(currentAmount+multiplier)"
+        currentAmount = currentAmount + multiplier
+        if isGameOver() {
+            logoImg.hidden = false
+            playBtn.hidden = false
+            theMltpl.hidden = false
+            addBtn.hidden = true
+            addLbl.hidden = true
+            theMltpl.text = ""
+        }
     }
-
+    
+    func isGameOver() -> Bool {
+        if currentAmount > maxAmount {
+            return true
+        } else {
+            return false
+        }
+    }
+    
 
 }
 
